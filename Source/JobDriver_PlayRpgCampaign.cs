d290d491b2ab5ff63cb6e6cc26c113d0049af535
Cheng-Yu Pai <mail4charliepai@gmail.com>
2023-08-21 00:27:30 -0700

    impl

---
+++ Source/JobDriver_PlayRpgCampaign.cs
@@ -0,0 +1,60 @@
+﻿using RimWorld;
+using Verse.AI;
+using Verse;
+using System;
+
+namespace BoardgameDev
+{
+    public class JobDriver_PlayRpgCampaign : JobDriver_SitFacingBuilding
+    {
+        private static readonly ThoughtDef playedRpgGame = DefDatabase<ThoughtDef>.GetNamed("PlayedRpgGame");
+
+        private Thing Campaign => TargetThingA;
+
+        protected override void ModifyPlayToil(Toil toil)
+        {
+            toil.AddFinishAction(delegate
+            {
+                pawn.needs.mood.thoughts.memories.TryGainMemory(
+                    ThoughtMaker.MakeThought(playedRpgGame, ChooseThoughtStage())
+                );
+                Campaign.TryGetComp<RpgCampaignComp>().PlayOnce();
+            });
+        }
+
+        private int ChooseThoughtStage()
+        {
+            var quality = Campaign.TryGetComp<CompQuality>();
+            int stage = 0;
+            switch (quality.Quality)
+            {
+                case QualityCategory.Awful:
+                    stage = Rand.ElementByWeight(0, 0.7f, 1, 0.2f, 2, 0.1f);
+                    break;
+                case QualityCategory.Poor:
+                    stage = Rand.ElementByWeight(0, 0.5f, 1, 0.3f, 2, 0.2f);
+                    break;
+                case QualityCategory.Normal:
+                    stage = Rand.ElementByWeight(0, 0.3f, 1, 0.3f, 2, 0.3f, 3, 0.1f);
+                    break;
+                case QualityCategory.Good:
+                    stage = Rand.ElementByWeight(0, 0.2f, 1, 0.2f, 2, 0.3f, 3, 0.2f, 4, 0.1f);
+                    break;
+                case QualityCategory.Excellent:
+                    stage = Rand.ElementByWeight(0, 0.1f, 1, 0.1f, 2, 0.3f, 3, 0.3f, 4, 0.2f);
+                    break;
+                case QualityCategory.Masterwork:
+                    stage = Rand.ElementByWeight(1, 0.1f, 2, 0.2f, 3, 0.3f, 4, 0.3f, 5, 0.1f);
+                    break;
+                case QualityCategory.Legendary:
+                    stage = Rand.ElementByWeight(2, 0.2f, 3, 0.3f, 4, 0.3f, 5, 0.2f);
+                    break;
+            }
+            if (Campaign.TryGetComp<RpgCampaignComp>().Finished)
+            {
+                stage = Math.Min(stage, 1);
+            }
+            return stage;
+        }
+    }
+}
---
+++ Source/RpgCampaignComp.cs
@@ -0,0 +1,80 @@
+﻿using RimWorld;
+using Verse;
+using System;
+
+namespace BoardgameDev
+{
+    public class RpgCampaignCompProperties : CompProperties
+    {
+        public RpgCampaignCompProperties()
+        {
+            compClass = typeof(RpgCampaignComp);
+        }
+    }
+
+    public class RpgCampaignComp : ThingComp
+    {
+        private int sessionsLeft = 20;
+
+        public int SessionsLeft => sessionsLeft;
+
+        public bool Finished => sessionsLeft == 0;
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Values.Look(ref sessionsLeft, "sessionsLeft", 20);
+        }
+
+        public void Init(QualityCategory quality, int sessionsPerColonist)
+        {
+            int colonists = PawnsFinder.AllMaps_FreeColonists.Count;
+            int colonistsAdjusted = (int)Math.Ceiling(Math.Pow(colonists, 0.8));
+
+            int qualityBonus = 1;
+            switch (quality)
+            {
+                case QualityCategory.Awful:
+                    qualityBonus = 1;
+                    break;
+                case QualityCategory.Poor:
+                    qualityBonus = 1;
+                    break;
+                case QualityCategory.Normal:
+                    qualityBonus = 1;
+                    break;
+                case QualityCategory.Good:
+                    qualityBonus = 2;
+                    break;
+                case QualityCategory.Excellent:
+                    qualityBonus = 2;
+                    break;
+                case QualityCategory.Masterwork:
+                    qualityBonus = 3;
+                    break;
+                case QualityCategory.Legendary:
+                    qualityBonus = 4;
+                    break;
+            }
+
+            sessionsLeft = colonistsAdjusted * sessionsPerColonist * qualityBonus;
+        }
+
+        public void PlayOnce()
+        {
+            sessionsLeft--;
+        }
+
+        public override string CompInspectStringExtra()
+        {
+            if (Finished)
+            {
+                return "Finished.";
+            }
+            else
+            {
+                return $"Sessions Left: {sessionsLeft}";
+            }
+        }
+    }
+}
